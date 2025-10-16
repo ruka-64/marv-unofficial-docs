41d60e947db21fa7f5d3639b6267a00413c26657
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-10-16 20:09:23 +0900

    prettier & formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,29 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'ガイド',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: '国を作る・国に参加する', slug: 'guides/towny-join' },
-					],
-				},
-				{
-					label: 'サーバー内の要素',
-					autogenerate: { directory: 'feature' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/withastro/starlight',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'ガイド',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: '国を作る・国に参加する', slug: 'guides/towny-join' },
+          ],
+        },
+        {
+          label: 'サーバー内の要素',
+          autogenerate: { directory: 'features' },
+        },
+      ],
+    }),
+  ],
 });
